3a9730a169a4062c74cd707495c1b89eb26aacce
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2021-06-05 20:05:04 +0200

    It was set all HTTPS addresses in .env as the json and was checked the incoming data from the server.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-
-
+import parsers
+from threading import Event
 if __name__ == '__main__':
+
+     print(parsers.bs)
+     Event().wait(5)
+     print('%' * 1700)
+     print(parsers.bs_1)
+#import wdb; wdb.set_trace()
--- parsers.py
+++ parsers.py
@@ -1,3 +1,13 @@
 from bs4 import BeautifulSoup
-from dotenv import load_dotenv
+from environs import Env
+from requests import get
+
+env = Env()
+env.read_env('./.env')
+
+page = get(env.json('URL_ADDRESSES')['ACCOMMODATION'])
+bs = BeautifulSoup(page.content, 'html.parser')
+
+page = get(env.json('URL_ADDRESSES')['I_WILL_BUY'])
+bs_1 = BeautifulSoup(page.content, 'html.parser')
 
