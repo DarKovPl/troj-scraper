486ba02a93fc1b6e3a2c7b2267217ff7114ebe52
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2021-06-24 20:00:30 +0200

    Class Links was renamed into RequestParameters and it has been configured to set headers and cookies from the session. Class UrlRequest inherits from RequestParameters and yields results into Class DataParser.

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from parsers import DataParser, Links, UrlRequests
+from parsers import DataParser, UrlRequest
 from threading import Event
 
 
 def main():
-    links = Links('.env').get_links_from_env()
-
-    for link in links:
-        server_response = UrlRequests(link).get_content()
-        page_content = DataParser(server_response).get_html_data()
+    for server_response in UrlRequest().get_content():
+        page_content = DataParser(server_response[0]).get_html_data()
+        request_header = server_response[1]
+        response_cookies = server_response[2]
 
         print('Start ' * 400)
-        print(page_content)
-        Event().wait(5)
+        # print(page_content)
+        print(request_header)
+        print(response_cookies)
         print('%' * 1700)
 
 
--- parsers.py
+++ parsers.py
@@ -1,32 +1,40 @@
 from bs4 import BeautifulSoup
 from environs import Env
-from requests import get
+from requests import Session
+from fake_useragent import UserAgent
+from threading import Event
 
 
-class Links:
+class RequestParameters:
 
-    def __init__(self, env_path: str):
-        self.env_path = env_path
+    def __init__(self):
+        self.env_path = '.env'
         self.env = Env()
+        self.user_agent = UserAgent()
+        self.session = Session()
 
     def get_links_from_env(self):
         self.env.read_env(self.env_path)
         url_dict = self.env.json('URL_ADDRESSES')
         urls = (url for url in url_dict.values())
-        for url in urls:
-            yield url
+        return urls
+
+    def get_user_agent_header(self):
+        headers = {'User-Agent': str(self.user_agent.random)}
+        return headers
+
+    def get_session_cookies(self):
+        cookies_ = self.session.cookies
+        return cookies_
 
 
-class UrlRequests:
-
-    def __init__(self, url: str, headers='', cookies=''):
-        self.url = url
-        self.headers = headers
-        self.cookies = cookies
+class UrlRequest(RequestParameters):
 
     def get_content(self):
-        response = get(self.url)
-        return response.content
+        for link in self.get_links_from_env():
+            response = self.session.get(link, headers=self.get_user_agent_header(), cookies=self.get_session_cookies())
+            yield response.content, response.request.headers, response.cookies
+            Event().wait(5)
 
 
 class DataParser:
